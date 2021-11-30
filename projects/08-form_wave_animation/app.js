c186a420fe5b63df1da6b4440810d965e7865ba9
Mohammad-Asif-Web <alexasifhhossain@gmail.com>
2021-11-30 17:38:07 +0600

    form wave animation modified

--- projects/08-form_wave_animation/app.js
+++ projects/08-form_wave_animation/app.js
@@ -1,13 +1,12 @@
 const labels = document.querySelectorAll('.form-control label');
 
-labels.forEach(label =>{
-    label.innerHTML = label.innerText.split('');
-    console.log(label);
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => 
+        `<span style="transition-delay:${idx * 60}ms">${letter}</span>`
+        ).join('');
 })
-
-
-
-
 
 
 
