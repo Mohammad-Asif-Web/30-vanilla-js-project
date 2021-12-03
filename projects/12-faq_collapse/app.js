dda9809d4494d8a8c525df79bdd737b5c4fc7991
Mohammad-Asif-Web <alexasifhhossain@gmail.com>
2021-12-03 20:34:47 +0600

    faq collapse project added

---
+++ projects/12-faq_collapse/app.js
@@ -0,0 +1,26 @@
+const faq = document.querySelectorAll('.faq');
+const icon = document.querySelectorAll('.icon');
+
+faq.forEach(para =>{
+    para.addEventListener('click',()=>{
+        para.classList.toggle('active')
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
